c1c805b096f4bbca933e77fa5bb4301edb0f8583
Joanne612 <joanneagnesian@gmail.com>
2026-03-28 14:54:54 +0700

    menambahkan class MesinKopi

--- Program.cs
+++ Program.cs
@@ -21,7 +21,55 @@
         Console.WriteLine("Masukkan nama paket:");
         string namaPaket = Console.ReadLine();
         paket.getKodePaket(namaPaket);
+
+        MesinKopi.Main2(args);
+
+    }
+}
+public class MesinKopi
+{
+    enum State { OFF, STANDBY, BREWING, MAINTENANCE };
+
+    State state = State.OFF;
+    string[] screenName = { "OFF", "STANDBY", "BREWING", "MAINTENANCE" };
+
+    public void standby()
+    {
+        State currentState = state;
+        state = State.STANDBY;
+        Console.WriteLine("Mesin " + currentState + " berubah menjadi " + screenName[(int)state]);
     }
 
+    public void brewing()
+    {
+        State currentState = state;
+        state = State.BREWING;
+        Console.WriteLine("Mesin " + currentState + " berubah menjadi " + screenName[(int)state]);
+    }
+    public void maintenance()
+    {
+        State currentState = state;
+        state = State.MAINTENANCE;
+        Console.WriteLine("Mesin " + currentState + " berubah menjadi " + screenName[(int)state]);
+    }
 
+    public static void Main2(string[] args)
+    {
+        MesinKopi mesin = new MesinKopi();
+        State state = State.OFF;
+        Console.WriteLine("Masukkan status mesin yang ingin dipilih:");
+        string statusMesin = Console.ReadLine();
+        if (statusMesin == "POWER_ON" || statusMesin == "POWER_OFF")
+        {
+            mesin.standby();
+        }
+        else if(statusMesin == "START_BREW" || statusMesin == "FINISH_BREW")
+        {
+            mesin.brewing();
+        }
+        else if (statusMesin == "START_MAINTENANCE" || statusMesin == "FINISH_MAINTENANCE")
+        {
+            mesin.maintenance();
+        }
+    }
 }
